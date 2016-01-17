aa3b1e615707c96140068bba3cf23d602e2ce67b
burtgulash <burtgulas@gmail.com>
2016-01-17 16:52:34 +0100

    compressed TST works.

--- tsticle.py
+++ tsticle.py
@@ -3,8 +3,8 @@
 
 class TNode:
 
-    def __init__(self, c):
-        self.c = c
+    def __init__(self, label):
+        self.label = label
         self.left = None
         self.mid = None
         self.right = None
@@ -19,13 +19,14 @@ class TernaryTrie:
         return self.size
 
     def find(self, word):
-        cur = self.root
+        prev = cur = self.root
         i = 0
 
         while i < len(word) and cur is not None:
-            if word[i] < cur.c:
+            prev = cur
+            if word[i] < cur.label:
                 cur = cur.left
-            elif word[i] > cur.c:
+            elif word[i] > cur.label:
                 cur = cur.right
             else:
                 if i == len(word) - 1:
@@ -42,9 +43,9 @@ class TernaryTrie:
 
         while cur is not None:
             prev = cur
-            if word[i] < cur.c:
+            if word[i] < cur.label:
                 cur = cur.left
-            elif word[i] > cur.c:
+            elif word[i] > cur.label:
                 cur = cur.right
             else:
                 if i == len(word) - 1:
@@ -62,9 +63,9 @@ class TernaryTrie:
             self.root = cur
         else:
             # find in which direction we should put the newly created subtree
-            if word[i] < prev.c:
+            if word[i] < prev.label:
                 prev.left = cur
-            elif word[i] > prev.c:
+            elif word[i] > prev.label:
                 prev.right = cur
             else:
                 prev.mid = cur
@@ -85,13 +86,128 @@ class TernaryTrie:
             prepend = lvl * "  "
             if n is None:
                 if direction == "-":
-                    s.append(prepend + "+ {"+acc+"}")
+                    s.append(prepend + ">> {"+acc+"}")
                 return
 
-            s.append(prepend + direction + " " + n.c)
+            s.append(prepend + direction + " " + n.label)
             _str(n.right, acc, lvl + 1, "/")
-            _str(n.mid, acc + n.c, lvl + 1, "-")
+            _str(n.mid, acc + n.label, lvl + 1, "-")
             _str(n.left, acc, lvl + 1, "\\")
+
+
+        _str(self.root, "", 0, "-")
+
+        return "\n".join(s)
+
+class TSTicle:
+
+    def __init__(self):
+        self.size = 0
+        self.root = None
+
+    def find(self, word):
+        word += '\0'
+        cur = self.root
+        i = 0
+
+        while cur is not None:
+            j = 0
+            while j < len(cur.label) - 1:
+                if word[i + j] != cur.label[j]:
+                    return False
+                j += 1
+
+            i += j
+            if word[i] < cur.label[j]:
+                cur = cur.left
+            elif word[i] > cur.label[j]:
+                cur = cur.right
+            else:
+                if i == len(word) - 1:
+                    return True
+                cur = cur.mid
+                i += 1
+
+        return False
+
+
+    def insert(self, word):
+        word += '\0'
+        prev = cur = self.root
+        c = None
+        i = 0
+
+        break_inner = False
+        while i < len(word) and cur is not None:
+            prev = cur
+            j = 0
+            while j < len(cur.label) - 1:
+                c = cur.label[j]
+                if word[i + j] != c:
+                    common_prefix = cur.label[:j + 1]
+                    label_suffix = cur.label[j + 1:]
+
+                    new = TNode(label_suffix)
+                    new.left = cur.left
+                    new.mid = cur.mid
+                    new.right = cur.right
+
+                    cur.label = common_prefix
+                    cur.left = None
+                    cur.mid = new
+                    cur.right = None
+
+                    break_inner = True
+                    break
+                j += 1
+
+            i += j
+            if break_inner:
+                break
+
+            c = cur.label[j]
+            if word[i] < c:
+                cur = cur.left
+            elif word[i] > c:
+                cur = cur.right
+            else:
+                if i == len(word) - 1:
+                    return True
+                cur = cur.mid
+                i += 1
+
+        # create first node of subtree
+        cur = TNode(word[i:])
+
+        # if root is empty, put subtree as root
+        if prev is None:
+            self.root = cur
+        else:
+            # find in which direction we should put the newly created subtree
+            if word[i] < c:
+                prev.left = cur
+            elif word[i] > c:
+                prev.right = cur
+            else:
+                prev.mid = cur
+
+        # increase size of the tree
+        self.size += 1
+
+
+    def __str__(self):
+        s = []
+        def _str(n, acc, lvl, direction):
+            prepend = lvl * "  "
+            if n is None:
+                if direction == "-":
+                    s.append(prepend + ">> {"+acc+"}")
+                return
+
+            s.append(prepend + direction + " " + n.label)
+            _str(n.right, acc + n.label[:-1], lvl + 1, "/")
+            _str(n.mid, acc + n.label, lvl + 1, "-")
+            _str(n.left, acc + n.label[:-1], lvl + 1, "\\")
 
 
         _str(self.root, "", 0, "-")
@@ -102,10 +218,13 @@ class TernaryTrie:
 
 
 
+
+
 if __name__ == "__main__":
-    t = TernaryTrie()
+    t = TSTicle()
     words = "autobus", "auto", "a", "kokotko", "autobusak", #"kkt", "kokot", "kk", "k"
     for w in words:
         t.insert(w)
         print(t)
         print(t.find(w), w)
+        print()
